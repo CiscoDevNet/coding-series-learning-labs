21d7628bc08fa8f2b66c1bb5101f40b456b005e3
Mike Maas <mikemaas@cisco.com>
2016-08-22 19:54:53 -0700

    Fixes for changes to MSE URL and API

--- labs/coding-202-parsing-json/get-ap-json.py
+++ labs/coding-202-parsing-json/get-ap-json.py
@@ -2,8 +2,8 @@ from urllib.request import Request, urlopen
 import json
 
 # Let's construct a request with headers
-req = Request('https://64.103.26.61/api/contextaware/v1/maps/info/Moscone/MosconeWest/DevNetLive')
-req.add_header('Authorization', 'Basic bGVhcm5pbmc6bGVhcm5pbmc==')
+req = Request('https://devnetapi.cisco.com/sandbox/mse/api/config/v1/maps/info/DevNetCampus/DevNetBuilding/DevNetZone')
+req.add_header('Authorization', 'Basic bGVhcm5pbmc6bGVhcm5pbmc=')
 req.add_header('Accept', 'application/json')
 
 # Actually open the request
@@ -17,13 +17,14 @@ responseString = response.read().decode("utf-8")
 #print(responseString)
 
 # Load the JSON string into a JSON object
-jsonObject = json.loads(responseString)
-print(json.dumps(jsonObject, sort_keys=True, indent=4))
+json_object = json.loads(response_string)
+#print(json_object)
+print(json.dumps(json_object, sort_keys=True, indent=4))
 
 # Only output what we are interested in - the AP name and IP address
-#accessPoints = jsonObject['Floor']['AccessPoint']
-#for ap in accessPoints:
-    #print('Access Point: ' + ap['name'] + '\t ip: ' + ap['ipAddress'])
+#access_points = json_object['accessPoints']
+#for ap in access_points:
+#    print('Access Point: ' + ap['name'] + '\t mac: ' + ap['radioMacAddress'])
 
 # Let's close the response we opened
 response.close()
